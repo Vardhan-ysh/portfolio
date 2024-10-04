67e37bb92f32d3b6f8d2a8f481e170df9effce07
Yashvardhan Singh <yashvardhansingh112233@gmail.com>
2024-10-04 14:57:02 +0530

    added data.dart and support for fonts

---
+++ portfolio/lib/data/data.dart
@@ -0,0 +1,88 @@
+// All needed data for the portfolio 😊
+
+const String aboutMe =
+    "Hi! I'm Yashvardhan Singh, an apprentice developer and third-year student pursuing B.Tech, specializing in cross-platform development. Passionate about the mobile ecosystem, I combine my studies with my practical experience to build intuitive and feature-rich applications. I aspire to put my skills to work on inspiring and challenging projects.";
+
+const myImage = 'assets/yashvardhan.jpg';
+
+const List<Map<String, String>> experiences = [
+  {
+    'title': 'Flutter Developer Intern',
+    'company': 'HelpyMotto',
+    'duration': 'Feburary 2024 - April 2024',
+    'description':
+        'Responsible for developing cross-platform mobile applications. Participated in code reviews and contributed to the improvement of the development process.'
+  },
+  {
+    'title': 'Flutter Developer',
+    'company': 'Infits Pvt. Ltd.',
+    'duration': 'May 2024 - August 2024',
+    'description':
+        'Worked on developing and maintaining mobile applications using Flutter. Collaborated with the design team to implement new features and improve user experience.'
+  },
+];
+
+const List<Map<String, String>> projects = [
+  {
+    'title': 'Kiosk App',
+    'description':
+        'A mobile application that helps users find the best motivational quotes to keep them inspired and motivated. The app is built using Flutter and Firebase.',
+    'link': 'null',
+    'image': 'assets/kiosk_app.png'
+  },
+  {
+    "title": "Chef’s Recipes",
+    'description':
+        'A mobile application that helps users find the best fitness trainers in their area. The app is built using Flutter and Firebase.',
+    'link': 'https://github.com/Vardhan-ysh/chefs_recipes',
+    'image': 'assets/chefs_recipes.png'
+  },
+  {
+    'title': 'Expense Tracker App',
+    'description':
+        'A mobile application that helps users find the best fitness trainers in their area. The app is built using Flutter and Firebase.',
+    'link': 'https://github.com/Vardhan-ysh/expense_tracker'
+  },
+];
+
+const List<Map<String, String>> skills = [
+  {'name': 'Flutter', 'image': 'assets/flutter.png'},
+  {'name': 'Dart', 'image': 'assets/dart.png'},
+  {'name': 'Firebase', 'image': 'assets/firebase.png'},
+  {'name': 'Version Control', 'image': 'assets/git.png'},
+  {'name': 'Node.js', 'image': 'assets/nodejs.png'},
+  {'name': 'Java', 'image': 'assets/java.png'},
+  {'name': 'Spring', 'image': 'assets/spring.png'},
+];
+
+const List<Map<String, String>> contact = [
+  {'type': 'Email', 'link': 'mailto:yashvardhansingh112233@gmail.com'},
+  {'type': 'LinkedIn', 'link': 'https://www.linkedin.com/in/vardhan-ysh/'},
+  {'type': 'GitHub', 'link': 'https://github.com/Vardhan-ysh'},
+  {'type': 'Twitter', 'link': 'https://x.com/vardhan_ysh'},
+  {'type': 'Instagram', 'link': 'https://www.instagram.com/vardhan_ysh/'},
+];
+
+const Map<String, String> education = {
+  'degree': 'B.Tech in Computer Science',
+  'university': 'JSSATE, Noida',
+  'duration': '2019 - 2023',
+};
+
+const String resumeLink =
+    'https://docs.google.com/document/d/1c5qv75GCsWkQUA5P8IGEwMA1SDHc8Td5/edit?usp=drivesdk&ouid=110193018134732468430&rtpof=true&sd=true';
+
+const List<Map<String, String>> achievementsAndCertificates = [
+  {
+    'title': 'SprintHacks Hackathon',
+    'description':
+        'Participated in the SprintHacks Hackathon and developed a mobile application that helps users find the best fitness trainers in their area.',
+        'link': 'https://github.com/Vardhan-ysh/upchaar'
+  },
+  {
+    'title': 'Flutter & Dart - The Complete Guide',
+    'description':
+        'Completed the Flutter Development Course on Udemy and built several projects to enhance my skills.',
+        'link': 'https://www.udemy.com/certificate/UC-8de9232e-99c5-4a89-a5b7-e0f38e7e6b8f/'
+  },
+];
