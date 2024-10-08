40aa1d4091ded34f3b565d4327885b04e1e60f8e
Yashvardhan Singh <yashvardhansingh112233@gmail.com>
2024-10-08 20:22:42 +0530

    build changes

---
+++ portfolio/install_flutter.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install Flutter
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+export PATH="$PATH:`pwd`/flutter/bin"
+
+# Ensure Flutter dependencies are installed
+flutter precache
